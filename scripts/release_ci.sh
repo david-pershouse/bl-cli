c9cffda8bcaed9f82a79a746a2e31bd8135ff2b4
david-pershouse <david.pershouse@mammoth.com.au>
2021-04-09 16:00:35 +1000

    try go get

--- scripts/release_ci.sh
+++ scripts/release_ci.sh
@@ -8,7 +8,7 @@ echo "generating changelog"
 current_dir=$(pwd)
 cd "$(mktemp -d)"
 
-go install github.com/digitalocean/github-changelog-generator
+go get -u github.com/digitalocean/github-changelog-generator
 
 tfile=$(mktemp)
 github-changelog-generator -org binarylane -repo bl-cli >"$tfile"
